c6ddd82d4d1264532ae5f56ca4d0037238af4787
Santiago Tumbaco <saantumb@espol.edu.ec>
2025-08-27 18:55:15 -0500

    feat(web): adding component accept invitation

---
+++ web/src/app/components/accept-invitation/accept-invitation.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accept-invitation',
+  imports: [],
+  templateUrl: './accept-invitation.html',
+  styleUrl: './accept-invitation.scss'
+})
+export class AcceptInvitationComponent {
+
+}
