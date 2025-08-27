ee6d45bdbdf7d775050c25ed63cc42269605da63
Santiago Tumbaco <saantumb@espol.edu.ec>
2025-08-27 18:55:41 -0500

    feat(web): adding invite staff

---
+++ web/src/app/components/invite-staff/invite-staff.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invite-staff',
+  imports: [],
+  templateUrl: './invite-staff.html',
+  styleUrl: './invite-staff.scss'
+})
+export class InviteStaffComponent {
+
+}
