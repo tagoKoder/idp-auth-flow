49c58ff607a4fe7f40719e749c953f958530a034
Santiago Tumbaco <saantumb@espol.edu.ec>
2025-08-27 22:45:10 -0500

    feat(app): plataffor info provider

---
+++ app/lib/core/utils/platform_info.dart
@@ -0,0 +1,27 @@
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:package_info_plus/package_info_plus.dart';
+
+class PlatformInfo {
+  static Future<Map<String, String>> headers() async {
+    final deviceInfo = DeviceInfoPlugin();
+    final pkg = await PackageInfo.fromPlatform();
+
+    String os = 'unknown', osVersion = '', model = '';
+    try {
+      final android = await deviceInfo.androidInfo;
+      os = 'android'; osVersion = '${android.version.release} (${android.version.sdkInt})';
+      model = '${android.manufacturer} ${android.model}';
+    } catch (_) {
+      final ios = await deviceInfo.iosInfo;
+      os = 'ios'; osVersion = ios.systemVersion ?? '';
+      model = ios.utsname.machine ?? '';
+    }
+
+    return {
+      'x-device-os': os,
+      'x-device-os-version': osVersion,
+      'x-device-model': model,
+      'x-app-version': '${pkg.version}+${pkg.buildNumber}',
+    };
+  }
+}
